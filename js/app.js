d119cf5b7f98f8c40638ee30616e9e1566a6c6fb
SusanneBrokaw <susannemarie@gmail.com>
2020-03-03 11:04:31 -0800

    Add working basic functionality - nav, highlights

--- js/app.js
+++ js/app.js
@@ -1,24 +1,76 @@
-
 // Get document sections and navbar UL
 const pageSections = document.querySelectorAll('section'); 
 const navList = document.querySelector('#navbar__list');
 
+
 // Create document fragment 
 const docFragment = document.createDocumentFragment();
 
-// Create LIs, anchor links, and add to fragment
+// Create LIs and anchor links. Add to fragment
 for (section of pageSections) {
     const newListItem = document.createElement('li');
     const newLink = document.createElement('a');
     newLink.textContent = section.getAttribute('data-nav');
     const linkAnchor = "#" + section.getAttribute('id');
     newLink.setAttribute('href', linkAnchor);
+    newLink.setAttribute('data-section', section.getAttribute('id'));
+    newLink.setAttribute('class', 'menu__link');
     newListItem.appendChild(newLink);
     docFragment.appendChild(newListItem);
 }
 
 // Put the fragment into the DOM inside the list 
 navList.appendChild(docFragment);
+
+// Callback function for highlighting section.
+// Runs whenever a section comes into view
+// Adds current-section class to things entering, removes from things exiting
+function highlightSection (entries, observer) {
+    const currentSection = entries[0].target;
+    const enteringView = entries[0].isIntersecting;
+    if (enteringView) {
+        currentSection.classList.add('current-section');
+    } else {
+        currentSection.classList.remove('current-section');
+    }
+}
+
+// Intersection Observer Options 
+const options = {
+    threshold: 0.5
+}
+
+// Create an intersection observer object
+const intObserver = new IntersectionObserver(highlightSection, options);
+
+// Target each section with observer
+for (section of pageSections) {
+    intObserver.observe(section);
+}
+
+// Nav event listener callback
+function clickNav(event) {
+    const section = event.target.getAttribute('data-section');
+    const sectionElement = document.getElementById(section);
+    event.preventDefault();
+    sectionElement.scrollIntoView({behavior: "smooth"});
+    // console.log(sectionElement.getClientRects());
+    // const location = sectionElement.getClientRects()[0].y;
+    // window.scrollTo({
+    //     top: location,
+    //     left: 0, 
+    //     behavior: 'smooth'
+    // });
+    // console.log(location);
+}
+
+// Create an event listener for nav links
+navList.addEventListener('click', clickNav);
+
+// Scroll to section
+// Get x/y of each section header
+// set an event on nav links
+// when event happens, scroll to anchor ID instead of using href
 
 /**
  * 
